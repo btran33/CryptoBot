acee929b46d01898abdea23cf83e5e886d4939e1
Ben Tran <btran33@illinois.edu>
2022-06-11 20:15:41 -0500

    Added helper functions for Position model and extra dependencies

--- src/models/position/index.js
+++ src/models/position/index.js
@@ -1,3 +1,5 @@
+const colors = require('colors/safe')
+
 class Position {
     constructor({ trade, id }) {
         this.state = 'open'
@@ -8,6 +10,31 @@ class Position {
     close({ trade }) {
         this.state = 'closed'
         this.exit = trade
+    }
+
+    profit() {
+        const fee = 0.0025
+        const entrance  = (this.enter.price) * (1 + fee)
+        const exit      = (this.exit.price)  * (1 - fee)
+        return exit - entrance
+    }
+
+    profitString() {
+        return this.profit().toFixed(2)
+    }
+
+    print() {
+        const enter = `Enter | ${colors.yellow(this.enter.price.toString())} | ${this.enter.time}`
+        const exit = this.exit ? `Exit | ${colors.magenta(this.exit.price.toString())} | ${this.exit.time}` : ''
+
+        var profit = ''
+        if (this.state === 'closed'){
+            const prof = `${this.profitString()}`
+            const colored = this.profit() > 0 ? colors.green(prof) : colors.red(prof)
+            profit = `Profit: ${colored}`
+        }
+
+        console.log(`${enter} --- ${exit} --- ${profit}`)
     }
 }
 
