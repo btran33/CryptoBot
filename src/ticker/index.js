5fdf33408f56ad0f8d2d6b8d6aa41554b7a3cce3
btran33 <btran33@illinois.edu>
2022-06-29 15:38:56 -0700

    Added a Ticker class that will be for live-feed of buy/sell offer on the market

---
+++ src/ticker/index.js
@@ -0,0 +1,50 @@
+const CoinbasePro = require('coinbase-pro')
+const config = require('../configuration')
+
+/**
+ * The class to set up a live feed on selling/trading of a product in the crypto market
+ */
+class Ticker {
+    constructor({ product, onTick, onError }) {
+        this.product = product
+        this.onTick = onTick
+        this.onError = onError
+        this.running = false
+    }
+
+    begin() {
+        this.running = true
+        this.client = new CoinbasePro.WebsocketClient(
+            [this.product],
+            config.get('COINBASE_WS_URL'),
+            null,
+            { 
+                channels : ['ticker', 'heartbeat'] 
+            }
+        );
+            
+        this.client.on('message', async data => {
+            if (data.type === 'ticker') {
+                await this.onTick(data)
+            }
+        })
+
+        this.client.on('error', err => {
+            this.onError(err)
+            this.client.connect()
+        })
+
+        this.client.on('close', () => {
+            if (this.running) {
+                this.client.connect()
+            }
+        })
+    }
+
+    stop() {
+        this.running = false
+        this.client.close()
+    }
+}
+
+module.exports = Ticker
